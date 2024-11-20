567af19a2cd2f38897344bf313e9a9a8c55551ad
noorjahan220 <noorjahanmeem220@gmail.com>
2024-11-20 21:08:51 +0600

    added banner img

--- src/Components/Banner/Banner.jsx
+++ src/Components/Banner/Banner.jsx
@@ -9,7 +9,7 @@ const Banner = () => {
 
     return (
         <div className=' mb-4 '>
-            <div className=' text-center bg-bannerColor pb-18 pt-5 px-10 lg:p-24   text-white rounded-bl-3xl rounded-br-3xl mb-6'>
+            <div className=' text-center bg-bannerColor pb-20 pt-5 px-10  lg:p-24   text-white rounded-bl-3xl rounded-br-3xl mb-10'>
                 <h2 className='text-2xl font-bold mb-3'>Upgrade Your Tech Accessorize with <br /> Gadget Heaven Accessories</h2>
                 <p className='text-sm font-normal mb-4'>Explore the latest gadgets that will take your experience to the next level. From smart devices to the coolest accessories, we have it all!</p>
                 <NavLink to='dashboard' className="py-2 px-4 rounded-[32px]  bg-white text-bannerColor text-xs font-bold" style={{
